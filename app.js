d4c4bca3a23c09343eead8bbbc7ec1873f272a8a
lprice93 <43084188+lprice93@users.noreply.github.com>
2018-10-30 16:02:04 -0500

    checking conditions

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 let friends = ["Halle", "Jasmine", "Kimani", "Haily", "Jalon"];
 
-let i = 99
-
 for (let x = 0; x < friends.length; x++) {
-    for (let i = 99; i > 0; i--) {
-        console.log(i + " lines of code in the file, " + i + " lines of code; " + friends[x] + " strikes one out, clears it all out, " + i-- + " lines of code in the file ");
-    } {
-        console.log[i];
+    for (let i = 99; i >= 0; i--) {
+        if (i > 1) {
+            console.log(i + " lines of code in the file, " + i + " lines of code; " + friends[x] + " strikes one out, clears it all out, " + (i - 1) + " lines of code in the file ");
+        } else if (i = 1) {
+            console.log(i + " line of code in the file, " + i + " line of code; " + friends[x] + " strikes one out, clears it all out, no more lines of code in the file ");
+        } else {
+            console.log(i + " lines of code in the file, " + i + " lines of code; " + friends[x] + " strikes them out, clears it all out, no more lines of code in the file ");
+        }
     }
 }
 
