86aeea8fc03afe1aef2d25872987004cc1ee939e
lprice93 <43084188+lprice93@users.noreply.github.com>
2018-10-30 14:56:59 -0500

    w/ nested loop

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-var friends = ["Halle", "Jasmine", "Kimani", "Haily", "Jalon"]
+let friends = ["Halle", "Jasmine", "Kimani", "Haily", "Jalon"];
 
-for (var i = 0; i < friends.length; i++) {
-    for (var x = 99; x < friends[i].length; i--) {
-        console.log(i + "lines of code in the file," + i + "lines of code;" + friends[i].length + "strikes one out, clears it all out," + i-- + "lines of code in the file");
+let i = 99
+
+for (let x = 0; x < friends.length; x++) {
+    for (let i = 99; i > 0; i--) {
+        console.log(i + " lines of code in the file, " + i + " lines of code; " + friends[x] + " strikes one out, clears it all out, " + i-- + " lines of code in the file ");
+    } {
+        console.log[i];
     }
 }
 
-// create loop with another loop nested inside so that each friend has a "99 lines of code in the file, 99 lines of code; John strikes one out, clears it all out, 98 lines of code in the file" and loop reiterates down until 0
-// use 'for' loop. loops through a block of code a number of times
-
-
-// i want the code to pass through each name in the friends array. and calculate new amount minus 1 until it reaches 0.
+// https://www.digitalocean.com/community/tutorials/how-to-construct-for-loops-in-javascript
